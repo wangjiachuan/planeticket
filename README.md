35a32b1735abdd6f19e7115b75cc78f9d8b68974
wangjiachuan <93126721@qq.com>
2016-03-23 11:02:53 +0800

    polish

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # planeticket
 This scripts only apply to python2.7
+use ElementTree to parse content.
+
+C:\Python27\python.exe D:\office\planeticket\planeticket.py
--- planeticket.py
+++ planeticket.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
-#coding=gbk
-import xml.etree.ElementTree as etree
 import urllib
 import sys
+try:
+    import xml.etree.cElementTree as etree
+except ImportError:
+    import xml.etree.ElementTree as etree
+    
+
 #import importlib
 #importlib.reload(sys)
 #sys.setdefaultencoding( "utf-8" )
-print(sys.getdefaultencoding())
+#print(sys.getdefaultencoding())
 
 context = urllib.urlopen('http://ws.qunar.com/holidayService.jcp?lane=上海-嘉峪关')
 tree = etree.parse(context)
 root = tree.getroot()
+
+
+#for elem in tree.iter():
+#    print (elem.tag, elem.attrib)
+
+
+
+for node in root[0]:
+    for child in node:
+        for child_detail in child.attrib.keys():
+            if child.attrib["type"] == "go" and int(child.attrib["price"])<1400:
+                print (child_detail,child.attrib[child_detail])
+
+                    
+'''
 for node in root[0]:
     if node.attrib["date"] == "2016-03-30":
+        print("find it")
         for child in node:
             for child_detail in child.attrib.keys():
-                if child.attrib["type"] == "go" and int(child.attrib["price"])<600:
+                if child.attrib["type"] == "go" and int(child.attrib["price"])<2600:
                     print (child_detail,child.attrib[child_detail])
+'''
                     
